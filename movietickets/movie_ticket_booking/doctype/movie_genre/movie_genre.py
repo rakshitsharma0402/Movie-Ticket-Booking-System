12cef258b509347370f7c89b9cd6d246052d8cf1
Rakshit Sharma <kumarrakshit0402@gmail.com>
2026-08-14 19:26:40 +0530

    feat(movie-genre): add case-insensitive uniqueness check

--- movietickets/movie_ticket_booking/doctype/movie_genre/movie_genre.py
+++ movietickets/movie_ticket_booking/doctype/movie_genre/movie_genre.py
@@ -1,22 +1,35 @@
 # Copyright (c) 2026, Rakshit Sharma and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class MovieGenre(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+    def validate(self):
+        self.validate_duplicate_genre_name()
 
-	from typing import TYPE_CHECKING
+    def validate_duplicate_genre_name(self):
+        """Case-insensitive check that no other Movie Genre record
+        shares this genre_name."""
+        if not self.genre_name:
+            return
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
+        existing = frappe.db.sql(
+            """
+            SELECT name
+            FROM `tabMovie Genre`
+            WHERE LOWER(genre_name) = LOWER(%s)
+              AND name != %s
+            """,
+            (self.genre_name, self.name or ""),
+        )
 
-		description: DF.SmallText | None
-		genre_name: DF.Data
-		is_active: DF.Check
-	# end: auto-generated types
-
-	_DOCTYPE_NAME = "Movie Genre"
+        if existing:
+            frappe.throw(
+                "A Movie Genre with the name '{0}' already exists ({1}). "
+                "Genre names must be unique regardless of case.".format(
+                    self.genre_name, existing[0][0]
+                ),
+                title="Duplicate Genre Name",
+            )
