36a1a46c79f9c17d6ff9d657dc4edd636a3ab717
Rakshit Sharma <kumarrakshit0402@gmail.com>
2026-08-17 18:44:44 +0530

    chore(fixtures): export booking_source custom field and Show sort property setter

--- movietickets/hooks.py
+++ movietickets/hooks.py
@@ -276,3 +276,15 @@ require_type_annotated_api_methods = True
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+#fixtures
+
+fixtures = [
+	{
+		"dt": "Custom Field",
+		"filters": [["dt", "=", "Ticket Booking"]],
+	},
+	{
+		"dt": "Property Setter",
+		"filters": [["doc_type", "=", "Show"]],
+	},
+]
